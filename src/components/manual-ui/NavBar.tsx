d4ba0491a4f6a2a27cbbcc6edf7cc726820c1bfc
Ghost9841 <asubedi778@gmail.com>
2025-11-23 20:49:49 +0545

    navbar

--- src/components/manual-ui/NavBar.tsx
+++ src/components/manual-ui/NavBar.tsx
@@ -69,13 +69,11 @@ export default function Navbar() {
             </DropdownMenuTrigger>
 
             <DropdownMenuContent align="end">
-              {user && (
                 <DropdownMenuItem asChild className="text-lg font-bold hover:underline">
                   <Link href="/createblog" >
                     <FilePlusCorner className="h-8! w-8! font-bold" /> New Blog
                   </Link>
                 </DropdownMenuItem>
-              )}
               <DropdownMenuItem asChild className="hover:underline">
                 <Link href="/myblogs">
                   My Blogs
